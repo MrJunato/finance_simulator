1087e29148729605e4c3434f5811ec1bd0178c5e
Renato Junior <47343193+MrJunato@users.noreply.github.com>
2022-12-26 12:29:17 -0300

    Removendo função clear_fields
    Essa função não estava sendo utilizada

--- app.py
+++ app.py
@@ -9,10 +9,6 @@ from datetime import datetime
 def diff_month(d1, d2):
     return (d1.year - d2.year) * 12 + d1.month - d2.month + 1
 
-def clear_fields():
-    ano_inicio.clear()
-    mes_inicio.clear()
-
 def main():
     ########## Sidebar ##########
     
